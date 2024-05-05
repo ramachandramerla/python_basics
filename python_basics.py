9c26c95accba9a7701b4fea66f3a7c62d02c43b0
Ramachandra M <merlachandu12@gmail.com>
2024-05-05 16:55:59 +0530

    all the necessaru allignment with the API key

--- python_basics.py
+++ python_basics.py
@@ -206,5 +206,18 @@ import json
 if len(sys.argv)!=2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json(),indent=2))
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=2&term=" + sys.argv[1])
+print(response.json())
+#just printing as we have them in the API key and understand it is difficult to understand the data 
+print("-----------------------------------------------------------------------------------------------")
+print(json.dumps(response.json(), indent=2))
+#using dumps creating a 2 spaces in the file representing 
+print("----------------------------------------------------------------------------------------------------")
+#if we need any specific things from the API like songs or something use below for loop and proper allignment in the file
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"], result["trackPrice"])
+    
+
+        
+
