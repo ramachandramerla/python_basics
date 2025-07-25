28da8509f3a78e255345e376d1f815203d90a84f
Ramachandra M <merlachandu12@gmail.com>
2025-07-25 08:29:23 +0530

    adding_new_oops_cncpt

---
+++ basics_revision_OOP.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+#create a class
+
+class Practice:
+
+    def __init__(self):
+        pass
+
+    # def reading(self, x,y):
+    #     self.x=x
+    #     self.y=y
+
+    def chandu(self,x,y):
+        return x*y
+    
+cha = Practice()
+a = int(input("provide for x : "))
+b = int(input("provide for y : "))
+
+print(cha.chandu(a,b))
+
+
+
+
